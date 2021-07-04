d160032d0bb671348208d747220896a8d3ae2257
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2021-07-04 11:40:04 +0000

    SCSI: mpt2sas: Fix for panic happening because of improper memory allocation

--- src/behaviour.rs
+++ src/behaviour.rs
@@ -11,3 +11,34 @@ use crate::compat::{CompatMessage, CompatProtocol, InboundMessage};
 use crate::protocol::{
     BitswapCodec, BitswapProtocol, BitswapRequest, BitswapResponse, RequestType,
 };
+use crate::query::{QueryEvent, QueryId, QueryManager, Request, Response};
+use crate::stats::*;
+use fnv::FnvHashMap;
+#[cfg(feature = "compat")]
+use fnv::FnvHashSet;
+use futures::{
+    channel::mpsc,
+    stream::{Stream, StreamExt},
+    task::{Context, Poll},
+};
+use libipld::{error::BlockNotFound, store::StoreParams, Block, Cid, Result};
+#[cfg(feature = "compat")]
+use libp2p::core::either::EitherOutput;
+use libp2p::core::{connection::ConnectionId, Multiaddr, PeerId};
+use libp2p::swarm::derive_prelude::{ConnectionClosed, DialFailure, FromSwarm, ListenFailure};
+#[cfg(feature = "compat")]
+use libp2p::swarm::{ConnectionHandlerSelect, NotifyHandler, OneShotHandler};
+use libp2p::{
+    request_response::{
+        InboundFailure, OutboundFailure, ProtocolSupport, RequestId, RequestResponse,
+        RequestResponseConfig, RequestResponseEvent, RequestResponseMessage, ResponseChannel,
+    },
+    swarm::{ConnectionHandler, NetworkBehaviour, NetworkBehaviourAction, PollParameters},
+};
+use prometheus::Registry;
+use std::{pin::Pin, time::Duration};
+
+/// Bitswap response channel.
+pub type Channel = ResponseChannel<BitswapResponse>;
+
+/// Event emitted by the bitswap behaviour.
