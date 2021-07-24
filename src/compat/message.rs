2e79f189763f33be5c4c121820c09508bbb12192
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2021-07-24 18:55:23 +0000

    read

--- src/compat/message.rs
+++ src/compat/message.rs
@@ -1,3 +1,5 @@
 use crate::compat::other;
 use crate::compat::prefix::Prefix;
-use crate::protocol::{BitswapRequest, BitswapResponse, R
+use crate::protocol::{BitswapRequest, BitswapResponse, RequestType};
+use libipld::Cid;
+use 
