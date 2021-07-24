dce3a45c2f70894efa8438400b4a26e3b85abe4b
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2021-07-24 21:05:31 +0000

    example usage

--- src/compat/message.rs
+++ src/compat/message.rs
@@ -2,4 +2,5 @@ use crate::compat::other;
 use crate::compat::prefix::Prefix;
 use crate::protocol::{BitswapRequest, BitswapResponse, RequestType};
 use libipld::Cid;
-use 
+use prost::Message;
+use std::convert::TryFrom
