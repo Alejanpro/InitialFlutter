3a198b2f5bb96974fbbf7c7591bc348323ee4993
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2021-10-13 20:43:53 +0000

    chore: upgrade dependencies

--- src/compat/message.rs
+++ src/compat/message.rs
@@ -85,4 +85,5 @@ impl CompatMessage {
         for payload in msg.payload {
             let prefix = Prefix::new(&payload.prefix)?;
             let cid = prefix.to_cid(&payload.data)?;
-            parts.
+            parts.push(CompatMessage::Response(
+           
