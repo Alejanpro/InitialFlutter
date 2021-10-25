d6d774703b3489e446354bcbdddeb17e9450ac7c
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2021-10-25 22:52:44 +0000

    Update config

--- src/compat/mod.rs
+++ src/compat/mod.rs
@@ -6,4 +6,4 @@ pub use message::CompatMessage;
 pub use protocol::{CompatProtocol, InboundMessage};
 
 fn other<E: std::error::Error + Send + Sync + 'static>(e: E) -> std::io::Error {
-    s
+    std::io::Error::new(std::io::ErrorKi
