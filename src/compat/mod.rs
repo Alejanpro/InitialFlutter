e4687546887e66170b3dfa442d88a599c99df980
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2021-10-26 10:27:22 +0000

    loplugin:stringconstant

--- src/compat/mod.rs
+++ src/compat/mod.rs
@@ -6,4 +6,5 @@ pub use message::CompatMessage;
 pub use protocol::{CompatProtocol, InboundMessage};
 
 fn other<E: std::error::Error + Send + Sync + 'static>(e: E) -> std::io::Error {
-    std::io::Error::new(std::io::ErrorKi
+    std::io::Error::new(std::io::ErrorKind::Other, e)
+}
