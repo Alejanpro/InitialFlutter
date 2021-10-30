df5260dea58f908963cb195862c628610865e965
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2021-10-30 18:21:14 +0000

    printing

--- src/compat/prefix.rs
+++ src/compat/prefix.rs
@@ -1,3 +1,5 @@
 use crate::compat::other;
 use libipld::cid::{Cid, Version};
-use libipld::multihash::{Code, Multih
+use libipld::multihash::{Code, MultihashDigest};
+use std::convert::TryFrom;
+use 
