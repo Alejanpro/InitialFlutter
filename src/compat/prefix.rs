c6a106dcec6dbaf5bbfc88c4d51cc88a29a2981b
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2021-10-31 11:52:30 +0000

    Update `README.md`.

--- src/compat/prefix.rs
+++ src/compat/prefix.rs
@@ -3,4 +3,6 @@ use libipld::cid::{Cid, Version};
 use libipld::multihash::{Code, MultihashDigest};
 use std::convert::TryFrom;
 use std::io::Result;
-use unsigned_varint::{decode as varint_decode, encode a
+use unsigned_varint::{decode as varint_decode, encode as varint_encode};
+
+/// Pre
