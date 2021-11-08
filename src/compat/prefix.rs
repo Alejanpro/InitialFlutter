167015ab06d4c1d3d4285a595a82649ed071c0b7
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2021-11-08 12:20:24 +0000

    add some test code

--- src/compat/prefix.rs
+++ src/compat/prefix.rs
@@ -24,4 +24,4 @@ impl Prefix {
         let (raw_version, remain) = varint_decode::u64(data).map_err(other)?;
         let version = Version::try_from(raw_version).map_err(other)?;
         let (codec, remain) = varint_decode::u64(remain).map_err(other)?;
-        let (mh_ty
+        let (mh_type, remain) = varint_decode::
