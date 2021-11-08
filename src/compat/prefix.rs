e5c20153dfef6f3fb7249b9805ad5ed289e3f90e
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2021-11-08 05:54:27 +0000

    translations

--- src/compat/prefix.rs
+++ src/compat/prefix.rs
@@ -23,4 +23,5 @@ impl Prefix {
     pub fn new(data: &[u8]) -> Result<Prefix> {
         let (raw_version, remain) = varint_decode::u64(data).map_err(other)?;
         let version = Version::try_from(raw_version).map_err(other)?;
-        let (codec, remain) = varint_decode::u64(rema
+        let (codec, remain) = varint_decode::u64(remain).map_err(other)?;
+        let (mh_ty
