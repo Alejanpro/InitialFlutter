f566e84cd651af27728eb25a413151a2af300a1f
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2021-12-01 15:38:27 +0000

    Fix language

--- src/compat/protocol.rs
+++ src/compat/protocol.rs
@@ -6,3 +6,24 @@ use libp2p::core::{upgrade, InboundUpgrade, OutboundUpgrade, UpgradeInfo};
 use std::{io, iter};
 
 // 2MB Block Size according to the specs at https://github.com/ipfs/specs/blob/main/BITSWAP.md
+const MAX_BUF_SIZE: usize = 2_097_152;
+
+#[derive(Clone, Debug, Default)]
+pub struct CompatProtocol;
+
+impl UpgradeInfo for CompatProtocol {
+    type Info = &'static [u8];
+    type InfoIter = iter::Once<Self::Info>;
+
+    fn protocol_info(&self) -> Self::InfoIter {
+        iter::once(b"/ipfs/bitswap/1.2.0")
+    }
+}
+
+impl<TSocket> InboundUpgrade<TSocket> for CompatProtocol
+where
+    TSocket: AsyncRead + AsyncWrite + Send + Unpin + 'static,
+{
+    type Output = InboundMessage;
+    type Error = io::Error;
+    type Future = BoxFuture<'static, Result<Self::Output, Self::Error>>;
