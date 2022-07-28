f67e76b2d124d182d2825da2124d0a969dba070a
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2022-07-28 13:08:48 +0000

    Rename for consistency.

--- src/query.rs
+++ src/query.rs
@@ -365,4 +365,4 @@ impl QueryManager {
 
     /// Processes the response of a have query.
     ///
-    /// Marks the in progress query as co
+    /// Marks the in progress query as complete and updates the set of peers that 
