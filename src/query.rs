0f2d9bb0ded3f80ecf34d3804f14235631725992
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2022-07-30 10:40:59 +0000

    fix broken unit test

--- src/query.rs
+++ src/query.rs
@@ -368,4 +368,5 @@ impl QueryManager {
     /// Marks the in progress query as complete and updates the set of peers that have
     /// a block. If there isn't an in progress block query a new block query will be
     /// started. If no block query can be started either a provider query is started or
-    /// the get query is marked as complete with a block-not
+    /// the get query is marked as complete with a block-not-found error.
+    fn recv_have(&mut 
