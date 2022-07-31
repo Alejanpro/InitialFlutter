c4407b9235a855eb32669463fc59448c0226beb3
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2022-07-31 05:53:00 +0000

    some minors

--- src/query.rs
+++ src/query.rs
@@ -370,4 +370,4 @@ impl QueryManager {
     /// started. If no block query can be started either a provider query is started or
     /// the get query is marked as complete with a block-not-found error.
     fn recv_have(&mut self, query: Header, peer_id: PeerId, have: bool) {
-        self.get_query
+        self.get_query(query.parent.unwrap(), |mgr, parent, m
