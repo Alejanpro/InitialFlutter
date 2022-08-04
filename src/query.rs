54bae83fdcc8216098ff261154f5dbee55ac8c00
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2022-08-04 15:40:12 +0000

    Initial file

--- src/query.rs
+++ src/query.rs
@@ -378,4 +378,5 @@ impl QueryManager {
             if have {
                 state.providers.push(peer_id);
             }
-            if state.block.is_none() && !state.providers.is_empt
+            if state.block.is_none() && !state.providers.is_empty() {
+                st
