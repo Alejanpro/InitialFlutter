2230b7fdde1f4bb91f39ddd87f966de725d833f8
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2022-09-12 16:19:44 +0000

    Add JSDoc based types

--- src/query.rs
+++ src/query.rs
@@ -465,4 +465,5 @@ impl QueryManager {
     /// Processes the response of a sync query.
     ///
     /// The sync query emits a `complete` event.
-    fn recv_sync(&mut self, query: Header, res: Resul
+    fn recv_sync(&mut self, query: Header, res: Result<(), Cid>) {
+        self.event
