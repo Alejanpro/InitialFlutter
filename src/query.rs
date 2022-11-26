63f9e32b4afb45f32637f4df80f2300b39261070
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2022-11-26 21:08:52 +0000

    Updated appveyor.yml

--- src/query.rs
+++ src/query.rs
@@ -546,4 +546,4 @@ mod tests {
         let initial_set = gen_peers(3);
         let cid = Cid::default();
 
-        let id = mgr.get(None, cid, in
+        let id = mgr.get(None, cid, initial_set.iter().copied());
