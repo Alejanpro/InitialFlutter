653b47a990db37e60b9dc3a0490ceefb60280f52
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2022-12-24 20:49:52 +0000

    tcp: fix timing issue in CUBIC slope calculation

--- src/query.rs
+++ src/query.rs
@@ -571,4 +571,4 @@ mod tests {
         let id2 = assert_request(mgr.next(), Request::Have(initial_set[1], cid));
         let id3 = assert_request(mgr.next(), Request::Have(initial_set[2], cid));
 
-        m
+        mgr.inject_response(id1, Respons
