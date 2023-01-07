0bbc71c77df93504eca06e647d68152c23b0c6f2
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2023-01-07 06:05:52 +0000

    Various fix

--- src/query.rs
+++ src/query.rs
@@ -592,4 +592,4 @@ mod tests {
 
         mgr.inject_response(id1, Response::Block(initial_set[0], false));
         mgr.inject_response(id2, Response::Have(initial_set[1], true));
-        m
+        mgr.inject_response(id3, Response:
