cd16b5bee81a6ad0ba01e91530135efd8f50a599
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2023-01-18 16:04:08 +0000

    Bump kramdown from 2.1.0 to 2.3.0

--- src/query.rs
+++ src/query.rs
@@ -612,4 +612,4 @@ mod tests {
         let id2 = assert_request(mgr.next(), Request::Have(initial_set[1], cid));
         let id3 = assert_request(mgr.next(), Request::Have(initial_set[2], cid));
 
-        mgr.inject_response(id1, Respon
+        mgr.inject_response(id1, Response::Block(initial_set[0], fal
