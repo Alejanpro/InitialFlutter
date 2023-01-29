b5cd7b289ec3fa31357c86f0173b96a8f6ecd9ad
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2023-01-29 07:00:58 +0000

    Fix #127

--- src/query.rs
+++ src/query.rs
@@ -613,4 +613,4 @@ mod tests {
         let id3 = assert_request(mgr.next(), Request::Have(initial_set[2], cid));
 
         mgr.inject_response(id1, Response::Block(initial_set[0], false));
-        mgr.inject_resp
+        mgr.inject_response(id2, Response::Hav
