c02e90df2151960c218fa9b244ff0696a8e2b917
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2023-01-30 10:44:01 +0000

    Correct spelling

--- src/query.rs
+++ src/query.rs
@@ -616,4 +616,4 @@ mod tests {
         mgr.inject_response(id2, Response::Have(initial_set[1], true));
         mgr.inject_response(id3, Response::Have(initial_set[2], true));
 
-        let id1 = assert_
+        let id1 = assert_request(mgr.next(), Req
