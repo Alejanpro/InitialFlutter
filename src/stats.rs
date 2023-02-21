0365ded1073ba16d27bec7389fcce8d546f8f2b5
Alejanpro <95589218+Alejanpro@users.noreply.github.com>
2023-02-21 18:12:21 +0000

    Update Chinese simplified translation

--- src/stats.rs
+++ src/stats.rs
@@ -23,3 +23,36 @@ lazy_static! {
         "bitswap_requests_canceled_total",
         "Number of canceled requests",
     )
+    .unwrap();
+    pub static ref BLOCK_NOT_FOUND: IntCounter = IntCounter::new(
+        "bitswap_block_not_found_total",
+        "Number of block not found errors.",
+    )
+    .unwrap();
+    pub static ref PROVIDERS_TOTAL: IntCounter = IntCounter::new(
+        "bitswap_providers_total",
+        r#"Number of providers total. Using the number of provider requests, the average
+        number of providers per request can be computed."#
+    )
+    .unwrap();
+    pub static ref MISSING_BLOCKS_TOTAL: IntCounter = IntCounter::new(
+        "bitswap_missing_blocks_total",
+        r#"Number of missing blocks total. Using the number of missing blocks requests, the
+        average number of missing blocks per request can be computed."#
+    )
+    .unwrap();
+    pub static ref RECEIVED_BLOCK_BYTES: IntCounter =
+        IntCounter::new("bitswap_received_block_bytes", "Number of received bytes.",).unwrap();
+    pub static ref RECEIVED_INVALID_BLOCK_BYTES: IntCounter = IntCounter::new(
+        "bitswap_received_invalid_block_bytes",
+        "Number of received bytes that didn't match the hash.",
+    )
+    .unwrap();
+    pub static ref SENT_BLOCK_BYTES: IntCounter =
+        IntCounter::new("bitswap_sent_block_bytes", "Number of sent block bytes.",).unwrap();
+    pub static ref RESPONSES_TOTAL: IntCounterVec = IntCounterVec::new(
+        Opts::new(
+            "bitswap_responses_total",
+            "Number of bitswap responses sent to peers.",
+        ),
+        &["type"],
